f22b0881e783e19d23fced018f0a0640d13f06d6
middleun <eunallaco@gmail.com>
2021-03-10 15:40:54 +0900

    210310_header navigation클릭시 page tilt 효과 완료

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+// page tilt effect
+$(function(){
+    var tiltNav={
+        $contents:$('.contentsWrap'),
+        $contentsFront:$('.contentsFront'),
+        $topBar:$('.topBar'),
+        offset:1800,
+        contentsHeight:$('.contentsFront').outerHeight(),
+        open:function(){
+            this.$contents.addClass('tilt');
+            this.$topBar.off('click');
+            $('.sectionWrap, .topBar').on('click',this.close.bind(this));
+            this.topBarFix(true);
+            console.log('opening');
+        },
+        close:function(){
+            this.$contents.removeClass('tilt');
+            $('.sectionWrap, .topBar',).off('click');
+            this.$topBar.on('click', this.open.bind(this));
+            this.topBarFix(false);
+            console.log('closing');
+
+
+        },
+        updateTransformOrigin:function(){
+            scrollTop = this.$contents.scrollTop();
+            equation = (scrollTop + this.offset) /this.contentsHeight*100;
+            this.$contentsFront.css('transform-origin','center' + equation +'%');
+        },
+
+        topBarFix:function(opening){
+            if(opening){
+                $(".topBar").css({
+                    // position:'absolute',
+                    top:this.$contents.scrollTop() + 30 + 'px'
+                });
+            }else{
+                setTimeout(function(){
+                    $(".topBar").css({
+                        // position:'fixed',
+                        top:'30px'
+                    });
+                }, 300);
+            }
+        },
+
+        bindEvents:function(){
+            this.$topBar.on('click',this.open.bind(this));
+            $('.hiddenTopBar').on('click', this.close.bind(this));
+            this.$contents.on('scroll',this.updateTransformOrigin.bind(this));
+        },
+        init:function(){
+            this.bindEvents();
+            this.updateTransformOrigin();
+        }
+            
+
+    };
+    tiltNav.init();
+
+
+});
